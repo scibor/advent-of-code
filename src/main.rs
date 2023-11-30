a724761bf5964b88d498b4a1e035d8a25e41391a
scibor <scibor.on.github@gmail.com>
2023-11-30 18:50:13 +0100

    Setup for 2023

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 #![allow(dead_code)]
 #![warn(clippy::all, clippy::pedantic)]
 
-mod y2017;
-mod y2018;
-mod y2019;
-mod y2020;
-mod y2022;
+mod y2023;
 
 fn main() {
-    let input = std::fs::read_to_string("inputs/y2022/day20.txt").unwrap();
-    println!("part1: {}", y2022::day20::part1(&input));
-    println!("part2: {}", y2022::day20::part2(&input));
+    let input = std::fs::read_to_string("inputs/y2023/day1.txt").unwrap();
+    println!("part1: {}", y2023::day1::part1(&input));
+    println!("part2: {}", y2023::day1::part2(&input));
 }
---
+++ src/y2023/day7.rs
@@ -0,0 +1,28 @@
+pub fn part1(_input: &str) -> String {
+    let result = 0;
+    format!("{result}")
+}
+
+pub fn part2(_input: &str) -> String {
+    let result = 0;
+    format!("{result}")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    const TEST_DATA: &str = "";
+
+    #[test]
+    fn test_case_part1() {
+        assert_eq!("1", part1(TEST_DATA));
+    }
+
+    #[test]
+    #[ignore = "not yet"]
+    fn test_case_part2() {
+        assert_eq!("1", part1(TEST_DATA));
+    }
+}
