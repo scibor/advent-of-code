a6ed1f2dea22c28dc69f182e17c7b4a674baef61
scibor <scibor.on.github@gmail.com>
2023-12-09 08:29:29 +0100

    2023-day9-part 1 & 2

--- src/y2023/day9.rs
+++ src/y2023/day9.rs
@@ -1,10 +1,63 @@
-pub fn part1(_input: &str) -> String {
-    let result = 0;
+fn is_list_constant(numbers: &[isize]) -> bool {
+    let first_value = numbers.first().unwrap();
+    numbers.iter().skip(1).all(|x| x == first_value)
+}
+
+fn process_number_list(numbers: &[isize], forward: bool) -> isize {
+    let mut lists: Vec<Vec<isize>> = Vec::new();
+    let mut current_list = numbers.to_vec().clone();
+    lists.push((numbers).to_vec());
+
+    while !is_list_constant(&current_list) {
+        let new_vec: Vec<isize> = current_list
+            .windows(2)
+            .map(|window| window.get(1).unwrap() - window.first().unwrap())
+            .collect();
+        lists.push(new_vec.clone());
+        current_list = new_vec;
+    }
+
+    let mut new_value = 0;
+    for list in lists.iter().rev() {
+        if forward {
+            new_value += list.last().unwrap();
+        } else {
+            new_value = list.first().unwrap() - new_value;
+        }
+    }
+
+    new_value
+}
+
+pub fn part1(input: &str) -> String {
+    let number_lines: Vec<Vec<isize>> = input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|number| number.parse::<isize>().unwrap())
+                .collect::<Vec<isize>>()
+        })
+        .collect();
+    let result: isize = number_lines
+        .iter()
+        .map(|line| process_number_list(line, true))
+        .sum();
     format!("{result}")
 }
 
-pub fn part2(_input: &str) -> String {
-    let result = 0;
+pub fn part2(input: &str) -> String {
+    let number_lines: Vec<Vec<isize>> = input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|number| number.parse::<isize>().unwrap())
+                .collect::<Vec<isize>>()
+        })
+        .collect();
+    let result: isize = number_lines
+        .iter()
+        .map(|line| process_number_list(line, false))
+        .sum();
     format!("{result}")
 }
 
@@ -13,16 +66,17 @@ mod tests {
     use super::*;
     use pretty_assertions::assert_eq;
 
-    const TEST_DATA: &str = "";
+    const TEST_DATA: &str = "0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45";
 
     #[test]
     fn test_case_part1() {
-        assert_eq!("1", part1(TEST_DATA));
+        assert_eq!("114", part1(TEST_DATA));
     }
 
     #[test]
-    #[ignore = "not yet"]
     fn test_case_part2() {
-        assert_eq!("1", part1(TEST_DATA));
+        assert_eq!(5, process_number_list(&vec![10, 13, 16, 21, 30, 45], false));
     }
 }
