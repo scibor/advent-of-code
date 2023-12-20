12bbae214fbba9725cf3478ff1975ce53c34990a
scibor <scibor.on.github@gmail.com>
2023-12-20 18:13:36 +0100

    y2023-day20-part 1 & 2

--- src/y2023/day20.rs
+++ src/y2023/day20.rs
@@ -31,8 +31,7 @@ struct Module {
 impl Module {
     fn handle_input(&mut self, pulse: &Pulse, source: String) {
         match (pulse, self.module_type.clone()) {
-            (_, ModuleType::Broadcast) => self.output = Some(pulse.clone()),
-            (_, ModuleType::TestType) => self.output = Some(pulse.clone()),
+            (_, ModuleType::Broadcast | ModuleType::TestType) => self.output = Some(pulse.clone()),
             (Pulse::High, ModuleType::FlipFlop(_)) => self.output = Some(Pulse::High),
             (Pulse::Low, ModuleType::FlipFlop(FlipFlopState::On)) => {
                 self.module_type = ModuleType::FlipFlop(FlipFlopState::Off);
@@ -44,10 +43,13 @@ impl Module {
             }
             (_, ModuleType::Conjunction { mut history }) => {
                 history.insert(source, pulse.clone());
+                self.module_type = ModuleType::Conjunction {
+                    history: history.clone(),
+                };
                 if history.values().all(|x| *x == Pulse::High) {
-                    self.output = Some(Pulse::Low)
+                    self.output = Some(Pulse::Low);
                 } else {
-                    self.output = Some(Pulse::High)
+                    self.output = Some(Pulse::High);
                 }
             }
         }
@@ -61,68 +63,96 @@ fn trim_name(name: &str) -> String {
     name.to_string()
 }
 
-fn process(name: String, pulse: Pulse, mut modules: Vec<Module>) -> Vec<Module> {
+fn is_everything_off(modules: &[Module]) -> bool {
+    let mut result = Vec::new();
+    for module in modules {
+        match &module.module_type {
+            ModuleType::Conjunction { history } => {
+                let turned_off = history.clone().values().all(|x| *x == Pulse::Low);
+                result.push((module.name.clone(), turned_off));
+            }
+            ModuleType::FlipFlop(state) => {
+                let turned_off = *state == FlipFlopState::Off;
+                result.push((module.name.clone(), turned_off));
+            }
+            _ => {}
+        }
+    }
+
+    result.iter().all(|(_, b)| *b)
+}
+
+fn process(name: &str, pulse: &Pulse, mut modules: Vec<Module>) -> (usize, usize, usize) {
     let mut low = 0;
     let mut high = 0;
 
+    // For test_case 2 in part 1
     modules.push(Module {
         name: "output".to_string(),
         module_type: ModuleType::TestType,
         connections: Vec::new(),
         output: None,
     });
+    // For real input
+    modules.push(Module {
+        name: "rx".to_string(),
+        module_type: ModuleType::TestType,
+        connections: Vec::new(),
+        output: None,
+    });
 
-    let mut queue: VecDeque<(String, Pulse, String)> = VecDeque::new();
-    queue.push_back((name, pulse.clone(), "button".to_string()));
+    let mut button_pushes = 0;
 
-    while !queue.is_empty() {
-        let (state, input, source) = queue.pop_front().unwrap();
-        println!("{:?}", &modules);
-        println!("{source} {:?} {state}", input);
-        let mut current_state = modules
-            .iter_mut()
-            .find(|x| x.name == state)
-            .unwrap()
-            .clone();
+    while button_pushes < 1000 && (button_pushes == 0 || !is_everything_off(&modules)) {
+        let mut queue: VecDeque<(String, Pulse, String)> = VecDeque::new();
+        queue.push_back((name.to_string(), pulse.clone(), "button".to_string()));
 
-        if input == Pulse::High {
-            high += 1;
-        } else {
-            low += 1;
+        while !queue.is_empty() {
+            let (state, input, source) = queue.pop_front().unwrap();
+            let mut current_state = modules
+                .iter_mut()
+                .find(|x| x.name == state)
+                .unwrap()
+                .clone();
+
+            if input == Pulse::High {
+                high += 1;
+            } else {
+                low += 1;
+            }
+
+            if std::mem::discriminant(&current_state.module_type)
+                == std::mem::discriminant(&ModuleType::FlipFlop(FlipFlopState::On))
+                && input == Pulse::High
+            {
+                continue;
+            }
+
+            current_state.handle_input(&input, source);
+            for connection in current_state.connections.clone() {
+                queue.push_back((
+                    connection.to_string(),
+                    current_state.output.as_ref().unwrap().clone(),
+                    current_state.name.clone(),
+                ));
+            }
+            modules = modules
+                .clone()
+                .into_iter()
+                .map(|x| {
+                    if x.name == current_state.name {
+                        current_state.clone()
+                    } else {
+                        x
+                    }
+                })
+                .collect();
         }
 
-        if std::mem::discriminant(&current_state.module_type)
-            == std::mem::discriminant(&ModuleType::FlipFlop(FlipFlopState::On))
-            && input == Pulse::High
-        {
-            continue;
-        }
-
-        current_state.handle_input(&input, source);
-        for connection in current_state.connections.clone() {
-            queue.push_back((
-                connection.to_string(),
-                current_state.output.as_ref().unwrap().clone(),
-                current_state.name.clone(),
-            ))
-        }
-        modules = modules
-            .clone()
-            .into_iter()
-            .map(|x| {
-                if x.name == current_state.name {
-                    current_state.clone()
-                } else {
-                    x
-                }
-            })
-            .collect();
+        button_pushes += 1;
     }
 
-    println!("low: {low}");
-    println!("high: {high}");
-
-    Vec::new()
+    (button_pushes, low, high)
 }
 
 fn find_sources(name: &str, input: &str) -> Vec<String> {
@@ -145,10 +175,10 @@ fn parse_line(line: &str, input: &str) -> Module {
     let potential_name = split.next().unwrap().trim();
     let name;
     let module_type;
-    if potential_name.starts_with("%") {
+    if potential_name.starts_with('%') {
         name = trim_name(potential_name);
         module_type = ModuleType::FlipFlop(FlipFlopState::Off);
-    } else if potential_name.starts_with("&") {
+    } else if potential_name.starts_with('&') {
         name = trim_name(potential_name);
         let sources = find_sources(&name, input);
         let mut history = HashMap::new();
@@ -184,15 +214,123 @@ fn parse_data(input: &str) -> Vec<Module> {
     input.lines().map(|x| parse_line(x, input)).collect()
 }
 
+/// # Panics
+#[must_use]
 pub fn part1(input: &str) -> String {
-    let mut modules = parse_data(input);
-    process("broadcaster".to_string(), Pulse::Low, modules);
-    let result = 0;
+    let modules = parse_data(input);
+    let (cycle_length, low, hi) = process("broadcaster", &Pulse::Low, modules);
+    let result = (1000 / cycle_length) * (1000 / cycle_length) * hi * low;
     format!("{result}")
 }
 
-pub fn part2(_input: &str) -> String {
-    let result = 0;
+fn process_part2(
+    name: &str,
+    pulse: &Pulse,
+    mut modules: Vec<Module>,
+    searched_cycle: &str,
+) -> usize {
+    // For test_case 2 in part 1
+    modules.push(Module {
+        name: "output".to_string(),
+        module_type: ModuleType::TestType,
+        connections: Vec::new(),
+        output: None,
+    });
+    // For real input
+    modules.push(Module {
+        name: "rx".to_string(),
+        module_type: ModuleType::TestType,
+        connections: Vec::new(),
+        output: None,
+    });
+
+    let mut button_pushes = 0;
+
+    'outer: loop {
+        let mut queue: VecDeque<(String, Pulse, String)> = VecDeque::new();
+        queue.push_back((name.to_string(), pulse.clone(), "button".to_string()));
+
+        while !queue.is_empty() {
+            let value = modules.iter().find(|x| x.name == searched_cycle).unwrap();
+            if value.output == Some(Pulse::High) {
+                break 'outer;
+            }
+            let (state, input, source) = queue.pop_front().unwrap();
+            let mut current_state = modules
+                .iter_mut()
+                .find(|x| x.name == state)
+                .unwrap()
+                .clone();
+
+            if std::mem::discriminant(&current_state.module_type)
+                == std::mem::discriminant(&ModuleType::FlipFlop(FlipFlopState::On))
+                && input == Pulse::High
+            {
+                continue;
+            }
+
+            current_state.handle_input(&input, source);
+            for connection in current_state.connections.clone() {
+                queue.push_back((
+                    connection.to_string(),
+                    current_state.output.as_ref().unwrap().clone(),
+                    current_state.name.clone(),
+                ));
+            }
+            modules = modules
+                .clone()
+                .into_iter()
+                .map(|x| {
+                    if x.name == current_state.name {
+                        current_state.clone()
+                    } else {
+                        x
+                    }
+                })
+                .collect();
+        }
+
+        button_pushes += 1;
+    }
+    println!("{searched_cycle} -> {button_pushes}");
+    button_pushes + 1
+}
+
+fn lcm(x: usize, y: usize) -> usize {
+    x * y / gcd(x, y)
+}
+
+fn gcd(mut x: usize, mut y: usize) -> usize {
+    let mut bigger = x;
+    let mut smaller = y;
+    if smaller > bigger {
+        std::mem::swap(&mut x, &mut y);
+    }
+
+    loop {
+        let res = bigger % smaller;
+        if res == 0 {
+            return smaller;
+        }
+
+        bigger = smaller;
+        smaller = res;
+    }
+}
+
+/// # Panics
+#[must_use]
+pub fn part2(input: &str) -> String {
+    let modules = parse_data(input);
+    // By watching my input I can see that rx is conjunction type module with 4 inputs: xn, qn, xf
+    // and zl. So I need to check cycles when they turn to be On at the same time.
+
+    let cycle1 = process_part2("broadcaster", &Pulse::Low, modules.clone(), "xn");
+    let cycle2 = process_part2("broadcaster", &Pulse::Low, modules.clone(), "qn");
+    let cycle3 = process_part2("broadcaster", &Pulse::Low, modules.clone(), "xf");
+    let cycle4 = process_part2("broadcaster", &Pulse::Low, modules.clone(), "zl");
+
+    let result = lcm(lcm(cycle1, cycle2), lcm(cycle3, cycle4));
     format!("{result}")
 }
 
@@ -214,7 +352,6 @@ mod tests {
     &con -> output";
 
     #[test]
-    #[ignore = "not yet"]
     fn test_case_part1_1() {
         assert_eq!("32000000", part1(TEST_DATA));
     }
@@ -222,11 +359,5 @@ mod tests {
     #[test]
     fn test_case_part1_2() {
         assert_eq!("11687500", part1(TEST_DATA2));
-    }
-
-    #[test]
-    #[ignore = "not yet"]
-    fn test_case_part2() {
-        assert_eq!("1", part1(TEST_DATA));
     }
 }
